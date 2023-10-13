0281a35dc2b67e784aa9f394c583a7e11d564983
GabrielRioo <gabrielpaivario@gmail.com>
2023-10-13 16:31:31 -0300

    Adicionando banco de dados e criando contexto

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,19 +1,42 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class FilmeController
+public class FilmeController : ControllerBase
 {
-	private static List<Filme> filmes = new List<Filme>();
+	private FilmeContext _context;
+
+	public FilmeController(FilmeContext context)
+	{
+		_context = context;
+	}
 
 	[HttpPost]
-	public void AdicionarFilme([FromBody] Filme filme)
+	public IActionResult AdicionarFilme([FromBody] Filme filme)
 	{
-		filmes.Add(filme);
-		Console.WriteLine(filme.Titulo);
-		Console.WriteLine(filme.Duracao);
+		_context.Filmes.Add(filme);
+		_context.SaveChanges();
+
+		// Informa o caminho que foi criado e o retorno da criação
+		return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+	}
+
+	[HttpGet]
+	public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+	{
+		return _context.Filmes.Skip(skip).Take(take);
+	}
+
+	[HttpGet("{id}")]
+	public IActionResult RecuperaFilmePorId(int id)
+	{
+		var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+		if (filme == null) return NotFound();
+
+		return Ok(filme);
 	}
 }
